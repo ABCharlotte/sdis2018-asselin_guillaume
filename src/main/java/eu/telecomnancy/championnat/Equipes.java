0993d4941b7ed8ea1e9d8f10659285cd9cbb4b06
charl04 <charles.valensole@gmail.com>
2018-12-06 17:34:18 +0100

    added lompok

--- src/main/java/eu/telecomnancy/championnat/Equipes.java
+++ src/main/java/eu/telecomnancy/championnat/Equipes.java
@@ -1,9 +1,12 @@
 package eu.telecomnancy.championnat;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
+import java.util.List;
+@Data
 @Entity
 public class Equipes {
 
@@ -12,7 +15,18 @@ public class Equipes {
     Long id;
     private String name;
 
+    public List <Matches> matches;
+
     Equipes(String name) {
         this.name = name;
     }
+
+    public void addMatches(Matches match){
+        this.matches.add(match);
+    }
+
+    public List<Matches> getMatches() {
+        return matches;
+    }
+
 }
