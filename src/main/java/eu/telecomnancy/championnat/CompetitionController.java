294e13d8e62011743e3c7f95fa12006a261dc521
ABCharlotte <charlotte.boulle@telecomnancy.net>
2018-12-07 13:21:19 +0100

    Controller vers localhost OK

--- src/main/java/eu/telecomnancy/championnat/CompetitionController.java
+++ src/main/java/eu/telecomnancy/championnat/CompetitionController.java
@@ -1,4 +1,54 @@
 package eu.telecomnancy.championnat;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
 public class CompetitionController {
+
+    private final CompetitionRepository repository;
+
+    CompetitionController(CompetitionRepository repository){
+        this.repository = repository;
+    }
+
+    //Agregate root
+    @GetMapping("/competitions")
+    List<Competition> all(){
+        return repository.findAll();
+    }
+
+    @PostMapping("competitions")
+    Competition newCompetition(@RequestBody Competition newCompetition){
+        return repository.save(newCompetition);
+    }
+
+    //Single item
+    @GetMapping("/competitions/{id}")
+    Competition one(@PathVariable Long id){
+        return repository.findById(id)
+                .orElseThrow(() -> new CompetitionNotFoundException(id));
+    }
+
+    @PutMapping("competitions/{id}")
+    Competition remplaceCompetition(@RequestBody Competition newCompetition, @PathVariable Long id){
+        return repository.findById(id)
+                .map(competition -> {
+                    competition.setName(newCompetition.getName());
+                    competition.setEquipes(newCompetition.getEquipes());
+                    return repository.save(competition);
+                })
+                .orElseGet(() -> {
+                    newCompetition.setId(id);
+                    return repository.save(newCompetition);
+                });
+    }
+
+    @DeleteMapping("/competitions/{id}")
+    void deleteEmployee(@PathVariable Long id){
+        repository.deleteById(id);
+    }
+
 }
