fc0e277eb4c036fdb4f02c71ac6c8bc17ce435bd
ABCharlotte <charlotte.boulle@telecomnancy.net>
2018-12-07 18:34:00 +0100

    PostMapping

--- src/main/java/eu/telecomnancy/championnat/CompetitionController.java
+++ src/main/java/eu/telecomnancy/championnat/CompetitionController.java
@@ -3,14 +3,18 @@ package eu.telecomnancy.championnat;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -28,8 +32,16 @@ public class CompetitionController {
     }
 
     //Agregate root
-    /*
-    @GetMapping("/competitions")
+    @RequestMapping(value = "/competitions", method = GET, produces = MediaType.APPLICATION_JSON_VALUE) //@GetMapping("/competitions")
+    Resources<Resource<Competition>> all() {
+
+        List<Resource<Competition>> competitions = repository.findAll().stream()
+                .map(assembler::toResource)
+                .collect(Collectors.toList());
+
+        return new Resources<>(competitions,
+                linkTo(methodOn(CompetitionController.class).all()).withSelfRel());
+    }/*@GetMapping("/competitions")
     Resources<Resource<Competition>> all() {
 
         List<Resource<Competition>> competitions = repository.findAll().stream()
@@ -41,29 +53,19 @@ public class CompetitionController {
         return new Resources<>(competitions,
                 linkTo(methodOn(CompetitionController.class).all()).withSelfRel());
     }*/
-    @RequestMapping(value = "/competitions", method = GET, produces = MediaType.APPLICATION_JSON_VALUE) //@GetMapping("/competitions")
-    Resources<Resource<Competition>> all() {
-
-        List<Resource<Competition>> competitions = repository.findAll().stream()
-                .map(assembler::toResource)
-                .collect(Collectors.toList());
-
-        return new Resources<>(competitions,
-                linkTo(methodOn(CompetitionController.class).all()).withSelfRel());
-    }
 
     @PostMapping("/competitions")
+    ResponseEntity<?> newCompetition(@RequestBody Competition newCompetition) throws URISyntaxException {
+        Resource<Competition> resource = assembler.toResource(repository.save(newCompetition));
+        return ResponseEntity
+                .created(new URI(resource.getId().expand().getHref()))
+                .body(resource);
+    }/*@PostMapping("/competitions")
     Competition newCompetition(@RequestBody Competition newCompetition){
         return repository.save(newCompetition);
-    }
+    }*/
 
     //Single item
-    //No RESTful
-    /*@GetMapping("/competitions/{id}")
-    Competition one(@PathVariable Long id){
-        return repository.findById(id)
-                .orElseThrow(() -> new CompetitionNotFoundException(id));
-    }*/
     @RequestMapping(value = "/competitions/{id}", method = GET, produces = MediaType.APPLICATION_JSON_VALUE) //@GetMapping("/competitions/{id}")
     Resource<Competition> one(@PathVariable Long id){
         Competition competition = repository.findById(id)
@@ -72,7 +74,12 @@ public class CompetitionController {
                 linkTo(methodOn(CompetitionController.class).one(id)).withSelfRel(),
                 linkTo(methodOn(CompetitionController.class).all()).withRel("competitions"));*/
         return assembler.toResource(competition);
-    }
+    }//No RESTful
+    /*@GetMapping("/competitions/{id}")
+    Competition one(@PathVariable Long id){
+        return repository.findById(id)
+                .orElseThrow(() -> new CompetitionNotFoundException(id));
+    }*/
 
     @PutMapping("/competitions/{id}")
     Competition remplaceCompetition(@RequestBody Competition newCompetition, @PathVariable Long id){
