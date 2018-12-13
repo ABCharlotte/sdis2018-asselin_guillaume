d98223cfeb232c3adcc72ac7182b3df35af350ed
charl04 <charles.valensole@gmail.com>
2018-12-13 10:46:37 +0100

    Addition de RabbitMQ, reste l'intégration.

--- src/main/java/eu/telecomnancy/championnat/ChampionnatApplication.java
+++ src/main/java/eu/telecomnancy/championnat/ChampionnatApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class ChampionnatApplication {
 
     public static void main(String... args) {
